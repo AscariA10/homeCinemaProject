368f12f49c2e151ff30c86df1c16dc7a94876cb3
Max Gerasymenko <geramaks03@gmail.com>
2023-01-27 23:45:51 +0200

    Added getGenres

---
+++ src/js-blocks/array-of-genres.js
@@ -0,0 +1,15 @@
+import { data } from "./fetch";
+
+let genresArray = [];
+genresArray.push(data);
+
+export const getGenres = (genresArray) => {
+    if (!genresArray || genresArray.length < 1) return '';
+    let genresString = '';
+
+    genresArray.forEach(genreId => {
+        const genre = genresArray.find(genre => genre.id === genreId);
+        genresString += genre.name + ', ';
+    });
+    return genresString.slice(0, genresString.length - 2);
+}
