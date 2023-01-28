8b65a84f8624fdcdfae7716a17501a301dfb9238
Max Gerasymenko <geramaks03@gmail.com>
2023-01-28 21:04:33 +0200

    Update array-of-genres.js

--- src/js-blocks/array-of-genres.js
+++ src/js-blocks/array-of-genres.js
@@ -3,18 +3,9 @@ import Api from "./fetch";
 const api = new Api();
 
 async function getGenres() {
-    const genres = await api.fetchGenres();
-    console.log('genres: ', genres);
+    const result = await api.fetchGenres();
+    const genres = result.genres.reduce(
+    (acc, genre) => ({ ...acc, [genre.id]: genre.name }),
+    []
+  );
 }
-getGenres();
-
-// export const getGenres = (genresArray) => {
-//     if (!genresArray || genresArray.length < 1) return '';
-//     let genresString = '';
-
-//     genresArray.forEach(genreId => {
-//         const genre = genresArray.find(genre => genre.id === genreId);
-//         genresString += genre.name + ', ';
-//     });
-//     return genresString.slice(0, genresString.length - 2);
-// }
