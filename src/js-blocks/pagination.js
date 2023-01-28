c0058c57c7b6e9c5f35bbed748a603a2b7af0051
Hatrison <vadimulanovskiy@gmail.com>
2023-01-29 01:39:52 +0200

    Refactored into class

--- src/js-blocks/pagination.js
+++ src/js-blocks/pagination.js
@@ -1,108 +1,112 @@
-const paginationPagesList = document.querySelector('.js-pages-list');
-const paginationArrowPrev = document.querySelector('.js-prev-btn');
-const paginationArrowNext = document.querySelector('.js-next-btn');
+export default class Pagination {
+  constructor(page, totalPages) {
+    this.page = page;
+    this.totalPages = totalPages;
 
-let page = 1;
-let totalPages = 11;
+    this.paginationPagesList = document.querySelector('.js-pages-list');
+    this.paginationArrowPrev = document.querySelector('.js-prev-btn');
+    this.paginationArrowNext = document.querySelector('.js-next-btn');
 
-paginationPagesList.addEventListener('click', onClick);
-paginationArrowPrev.addEventListener('click', onPrev);
-paginationArrowNext.addEventListener('click', onNext);
+    this.paginationPagesList.addEventListener('click', this.onClick);
+    this.paginationArrowPrev.addEventListener('click', this.onPrev);
+    this.paginationArrowNext.addEventListener('click', this.onNext);
 
-function onPrev(event) {
-  page--;
-  createPagination(page, totalPages);
-}
-
-function onNext(event) {
-  page++;
-  createPagination(page, totalPages);
-}
-
-function onClick(event) {
-  const classes = [...event.target.classList];
-  if (classes.includes('pagination__dots')) return;
-  if (classes.includes('pagination__list')) return;
-  if (classes.includes('pagination__number_current')) return;
-
-  page = Number(event.target.innerText);
-  createPagination(page, totalPages);
-}
-
-function createPagination(page, totalPages) {
-  let markup = '';
-
-  let beforeCurr = page - 2;
-  let afterCurr = page + 2;
-
-  if (page === 1) {
-    afterCurr += 2;
-  } else if (page === 2) {
-    afterCurr += 1;
+    this.createPagination(this.page, this.totalPages);
   }
 
-  if (page === totalPages) {
-    beforeCurr -= 2;
-  } else if (page === totalPages - 1) {
-    beforeCurr -= 1;
-  }
+  onPrev = event => {
+    this.page--;
+    this.createPagination(this.page, this.totalPages);
+  };
 
-  if (page > 1) {
-    paginationArrowPrev.classList.remove('visually-hidden');
-  } else {
-    paginationArrowPrev.classList.add('visually-hidden');
-  }
+  onNext = event => {
+    this.page++;
+    this.createPagination(this.page, this.totalPages);
+  };
 
-  if (page >= 4) {
-    markup += `<li>
+  onClick = event => {
+    this.classes = [...event.target.classList];
+    if (this.classes.includes('pagination__dots')) return;
+    if (this.classes.includes('pagination__list')) return;
+    if (this.classes.includes('pagination__number_current')) return;
+
+    this.page = Number(event.target.innerText);
+    this.createPagination(this.page, this.totalPages);
+  };
+
+  createPagination(page, totalPages) {
+    let markup = '';
+
+    let beforeCurr = page - 2;
+    let afterCurr = page + 2;
+
+    if (page === 1) {
+      afterCurr += 2;
+    } else if (page === 2) {
+      afterCurr += 1;
+    }
+
+    if (page === totalPages) {
+      beforeCurr -= 2;
+    } else if (page === totalPages - 1) {
+      beforeCurr -= 1;
+    }
+
+    if (page > 1) {
+      this.paginationArrowPrev.classList.remove('visually-hidden');
+    } else {
+      this.paginationArrowPrev.classList.add('visually-hidden');
+    }
+
+    if (page >= 4) {
+      markup += `<li>
         <button type="button" class="pagination__item pagination__number">
           1
         </button>
       </li>`;
 
-    if (page >= 5) {
-      markup += `<li class="pagination__item pagination__dots">...</li>`;
+      if (page >= 5) {
+        markup += `<li class="pagination__item pagination__dots">...</li>`;
+      }
     }
-  }
 
-  for (let pageNumber = beforeCurr; pageNumber <= afterCurr; pageNumber++) {
-    if (pageNumber <= 0 || pageNumber > totalPages) continue;
+    for (let pageNumber = beforeCurr; pageNumber <= afterCurr; pageNumber++) {
+      if (pageNumber <= 0 || pageNumber > totalPages) continue;
 
-    if (pageNumber === page) {
-      markup += `<li>
+      if (pageNumber === page) {
+        markup += `<li>
         <button type="button" class="pagination__item pagination__number pagination__number_current">
           ${pageNumber}
         </button>
       </li>`;
-      continue;
-    }
+        continue;
+      }
 
-    markup += `<li>
+      markup += `<li>
         <button type="button" class="pagination__item pagination__number">
           ${pageNumber}
         </button>
       </li>`;
-  }
-
-  if (page <= totalPages - 3) {
-    if (page <= totalPages - 4) {
-      markup += `<li class="pagination__item pagination__dots">...</li>`;
     }
 
-    markup += `<li>
+    if (page <= totalPages - 3) {
+      if (page <= totalPages - 4) {
+        markup += `<li class="pagination__item pagination__dots">...</li>`;
+      }
+
+      markup += `<li>
         <button type="button" class="pagination__item pagination__number">
           ${totalPages}
         </button>
       </li>`;
-  }
+    }
 
-  if (page === totalPages) {
-    paginationArrowNext.classList.add('visually-hidden');
-  } else {
-    paginationArrowNext.classList.remove('visually-hidden');
-  }
+    if (page === totalPages) {
+      this.paginationArrowNext.classList.add('visually-hidden');
+    } else {
+      this.paginationArrowNext.classList.remove('visually-hidden');
+    }
 
-  paginationPagesList.innerHTML = markup;
+    this.paginationPagesList.innerHTML = markup;
+  }
 }
-
-createPagination(page, totalPages);
