d41808abef36a686229b8a777a2ebb5bd1d8e2fa
VolodymyrKravchenko <109697703+AscariA10@users.noreply.github.com>
2023-01-29 00:05:39 +0200

    Library page added, scripts don`t falldawn

--- src/js-blocks/modal-window.js
+++ src/js-blocks/modal-window.js
@@ -7,57 +7,52 @@ closeModalBtnRef.addEventListener('click', onCloseModal);
 backdrop.addEventListener('click', onBackdrop);
 
 function onOpenModal() {
-  window.addEventListener('keydown', onEscPress);
-  document.body.classList.add('show-modal');
+   window.addEventListener('keydown', onEscPress);
+   document.body.classList.add('show-modal');
 }
 function onCloseModal() {
-  window.removeEventListener('keydown', onEscPress);
-  document.body.classList.remove('show-modal');
+   window.removeEventListener('keydown', onEscPress);
+   document.body.classList.remove('show-modal');
 }
 function onBackdrop(evt) {
-  if (evt.currentTarget === evt.target) {
-    onCloseModal();
-  }
+   if (evt.currentTarget === evt.target) {
+      onCloseModal();
+   }
 }
-function onEscPress(evt){
-  if(evt.code === 'Escape'){
-    onCloseModal();
-  }
+function onEscPress(evt) {
+   if (evt.code === 'Escape') {
+      onCloseModal();
+   }
 }
 
 //смена названия кнопки watch
- const watchBtnRef = document.querySelector('.btn_watched');
- const watchSpanEl = document.querySelector('.change_watch');
- watchSpanEl.addEventListener('click', onChangeTitleWatch);
+const watchBtnRef = document.querySelector('.btn_watched');
+const watchSpanEl = document.querySelector('.change_watch');
+watchSpanEl.addEventListener('click', onChangeTitleWatch);
 
- function onChangeTitleWatch(evt) {
-    if (watchSpanEl.textContent == "add to Watched") {
-     watchSpanEl.textContent = "remove from Watched";
+function onChangeTitleWatch(evt) {
+   if (watchSpanEl.textContent == 'add to Watched') {
+      watchSpanEl.textContent = 'remove from Watched';
+   } else {
+      watchSpanEl.textContent = 'add to Watched';
    }
-   else {
-     watchSpanEl.textContent = "add to Watched";
-   }
-  //  setTimeout(() => {
-  //   watchBtnRef.style.cssText = 'backgroundColor: #fff; color: #000; border: 1px solid black';
-  //  }, 1000);
-
-   }
+   //  setTimeout(() => {
+   //   watchBtnRef.style.cssText = 'backgroundColor: #fff; color: #000; border: 1px solid black';
+   //  }, 1000);
+}
 //смена названия кнопки queue
- const queueBtnRef = document.querySelector('.btn_queue');
- const queueSpanEl = document.querySelector('.change_queue');
- queueBtnRef.addEventListener('click', onChangeTitleQueue);
+const queueBtnRef = document.querySelector('.btn_queue');
+const queueSpanEl = document.querySelector('.change_queue');
+queueBtnRef.addEventListener('click', onChangeTitleQueue);
 
 function onChangeTitleQueue() {
-   if (queueSpanEl.textContent == "add to queue") {
-    queueSpanEl.textContent = "remove from queue";
+   if (queueSpanEl.textContent == 'add to queue') {
+      queueSpanEl.textContent = 'remove from queue';
+   } else {
+      queueSpanEl.textContent = 'add to queue';
    }
-   else {
-     queueSpanEl.textContent = "add to queue"
-   }
- }
+}
 //заполнение модалки информацией
 
 //кнопки переключатели
- //const LocalStorageEntry = new LocalStorageEntry(key);
-
- 
+//const LocalStorageEntry = new LocalStorageEntry(key);
