807f4d202759ef35e8698eb4e8298b48064e5a4c
Max Gerasymenko <geramaks03@gmail.com>
2023-01-28 18:30:30 +0200

    get massive objects

--- src/js-blocks/array-of-genres.js
+++ src/js-blocks/array-of-genres.js
@@ -1,15 +1,20 @@
-import { data } from "./fetch";
+import Api from "./fetch";
 
-let genresArray = [];
-genresArray.push(data);
+const api = new Api();
 
-export const getGenres = (genresArray) => {
-    if (!genresArray || genresArray.length < 1) return '';
-    let genresString = '';
-
-    genresArray.forEach(genreId => {
-        const genre = genresArray.find(genre => genre.id === genreId);
-        genresString += genre.name + ', ';
-    });
-    return genresString.slice(0, genresString.length - 2);
+async function getGenres() {
+    const genres = await api.fetchGenres();
+    console.log('genres: ', genres);
 }
+getGenres();
+
+// export const getGenres = (genresArray) => {
+//     if (!genresArray || genresArray.length < 1) return '';
+//     let genresString = '';
+
+//     genresArray.forEach(genreId => {
+//         const genre = genresArray.find(genre => genre.id === genreId);
+//         genresString += genre.name + ', ';
+//     });
+//     return genresString.slice(0, genresString.length - 2);
+// }
