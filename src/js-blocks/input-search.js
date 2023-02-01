f5e0d6a65bf5d310d1ba3be537d0bc13dd50c776
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-02-01 23:35:22 +0200

    input-search

--- src/js-blocks/input-search.js
+++ src/js-blocks/input-search.js
@@ -1,5 +1,107 @@
-import { renderCard } from './card-draw';
-import ApiMovies from './fetch';
+// import { cardList } from './card-draw.js';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import ApiMovies from './fetch.js';
+// import Pagination from './pagination.js';
+
+// const apiMovies = new ApiMovies();
+
+// const galleryList = document.querySelector('.gallery-list');
+// const errorMessage = document.querySelector('.warning-message');
+// const formInput = document.querySelector('.js-search__form');
+
+// formInput.addEventListener('submit', onSearchMovies);
+
+// function onSearchMovies(evt) {
+//   evt.preventDefault();
+//   const value = evt.currentTarget.elements.searchInput.value.trim();
+//   //   console.log(value);
+
+//   if (value) {
+//     clearMovieCardContainer();
+
+//     apiMovies.pageNumber = 1;
+//     apiMovies.searchMovieByName(value).then(data => {
+//       // console.log(data);
+
+//       if (!data.length) {
+//         errorMessage.classList.remove('hidden');
+//         errorMessage.textContent =
+//           'No matches found for your query. Enter the properly name of the movie.';
+//         clearMessage();
+
+//         apiMovies.fetchTrendMovies().then(() => {
+//           const pagination = new Pagination();
+//           pagination.setFunction(apiMovies.fetchTrendMovies, apiMovies);
+//           formInput.reset();
+//         });
+//       } else {
+//         const pagination = new Pagination();
+//         console.log(pagination);
+//         pagination.setFunction(apiMovies.searchMovieByName, apiMovies, value);
+//         //   cardList(data);
+//         formInput.reset();
+//       }
+//     });
+//   } else {
+//     errorMessage.classList.remove('hidden');
+//     errorMessage.textContent = 'Please, enter movie name to search.';
+//     clearMessage();
+//     return;
+//   }
+
+//   //-------------------------------------------------------------
+//   // //  apiMovies.query = formInput.ariaValueMax.trim();
+//   // event.preventDefault();
+//   // errorMessage.classList.remove('.hidden');
+
+//   // console.log(formInput.value);
+
+//   // if (apiMovies.query !== '') {
+//   //    apiMovies
+//   //       .searchMovieByName()
+//   //       .then(films => {
+//   //          if (films.results.length < 1) {
+//   //             errorMessage.classList.remove('.hidden');
+
+//   //             setTimeout(() => {
+//   //                errorMessage.classList.add('.hidden');
+//   //             }, 3000);
+//   //             return;
+//   //          }
+//   //          if (films.results.length > 1) {
+//   //             errorMessage.classList.add('.hidden');
+
+//   //             clearMovieCardContainer();
+
+//   //             cardMarkup(films.results);
+
+//   //             clearInput();
+//   //          }
+//   //       })
+//   //       .catch(err => console.log(err));
+//   // }
+//   //----------------------------------------------------------------
+// }
+// // function clearInput() {
+// //   formInput.innerHTML = '';
+// // }
+// function clearMovieCardContainer() {
+//   galleryList.innerHTML = '';
+// }
+// // async function cardMarkup(data) {
+// //   const markup = cardList(data).map(renderCard).join('');
+// //   galleryList.innerHTML = markup;
+// // }
+// function clearMessage() {
+//   setTimeout(() => {
+//     errorMessage.classList.add('hidden');
+//   }, 3000);
+// }
+//-------------------------Вадим-----------------------------------
+// import { cardList } from './card-draw.js';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import ApiMovies from './fetch.js';
+import Pagination from './pagination.js';
 
 const apiMovies = new ApiMovies();
 
@@ -9,45 +111,58 @@ const formInput = document.querySelector('.js-search__form');
 
 formInput.addEventListener('submit', onSearchMovies);
 
-function onSearchMovies(event) {
-   //  apiMovies.query = formInput.ariaValueMax.trim();
-   event.preventDefault();
-   errorMessage.classList.remove('.hidden');
+function onSearchMovies(evt) {
+  evt.preventDefault();
+  const value = evt.currentTarget.elements.searchInput.value.trim();
 
-   console.log(formInput.value);
+  if (value) {
+    //  clearMovieCardContainer();
 
-   if (apiMovies.query !== '') {
-      apiMovies
-         .searchMovieByName()
-         .then(films => {
-            if (films.results.length < 1) {
-               errorMessage.classList.remove('.hidden');
+    //  apiMovies.pageNumber = 1;
+    const pagination = new Pagination(true);
 
-               setTimeout(() => {
-                  errorMessage.classList.add('.hidden');
-               }, 3000);
-               return;
-            }
-            if (films.results.length > 1) {
-               errorMessage.classList.add('.hidden');
+    pagination.setFunction(apiMovies.searchMovieByName, apiMovies, value);
+    formInput.reset();
 
-               clearMovieCardContainer();
+    //  apiMovies.searchMovieByName(value).then(data => {
+    //    // console.log(data);
+    //    if (!data.length) {
+    //      errorMessage.classList.remove('hidden');
+    //      errorMessage.textContent =
+    //        'Search result not successful. Enter the correct movie title.';
+    //      pagination.setFunction(apiMovies.fetchTrendMovies, apiMovies);
 
-               cardMarkup(films.results);
+    //      clearMessage();
+    //    } else {
+    //      pagination.setFunction(apiMovies.searchMovieByName, apiMovies, value);
+    //    }
 
-               clearInput();
-            }
-         })
-         .catch(err => console.log(err));
-   }
+    //    formInput.reset();
+    //  });
+  } else {
+    errorMessage.classList.remove('hidden');
+    errorMessage.textContent = 'Please, enter movie name to search.';
+    clearMessage();
+    return;
+  }
 }
-function clearInput() {
-   formInput.innerHTML = '';
+
+// function clearMovieCardContainer() {
+//   galleryList.innerHTML = '';
+// }
+
+function clearMessage() {
+  setTimeout(() => {
+    errorMessage.classList.add('hidden');
+  }, 3000);
 }
-function clearMovieCardContainer() {
-   galleryList.innerHTML = '';
-}
-async function cardMarkup(data) {
-   const markup = cardList(data).map(renderCard).join('');
-   galleryList.innerHTML = markup;
+
+export function fetchTrendFilms() {
+  errorMessage.classList.remove('hidden');
+  errorMessage.textContent =
+    'Search result not successful. Enter the correct movie title.';
+  const pagination = new Pagination(true);
+  pagination.setFunction(apiMovies.fetchTrendMovies, apiMovies);
+
+  clearMessage();
 }
--- src/js-blocks/pagination.js
+++ src/js-blocks/pagination.js
@@ -2,6 +2,7 @@
 import { cardList } from './card-draw.js';
 import { LocalStorageEntry } from './localStorageEntry.js';
 const localStorageCurrentPage = new LocalStorageEntry('current_page_number');
+import { fetchTrendFilms } from './input-search.js';
 
 // To create a pagination you have to create an instance of class Pagination
 // and call setFunction method with relevant arguments
@@ -37,6 +38,8 @@ export default class Pagination {
    * Third and further params - args of requested function
    */
   setFunction = async (fn, ...arrOfArgs) => {
+    this.page = 1;
+
     this.fn = fn.bind(...arrOfArgs);
     this.linkToIntance = arrOfArgs[0];
     this.#render();
@@ -74,7 +77,12 @@ export default class Pagination {
     this.#hidePagination();
 
     this.linkToIntance.pageNumber = this.page;
+
     this.res = await this.fn();
+    console.log(this.res);
+
+    if (!this.res.length) fetchTrendFilms();
+
     this.totalPages = this.linkToIntance.totalPagesNumber;
     cardList(this.res);
 
@@ -203,3 +211,210 @@ const api = new ApiMovies();
   //   pagination.setFunction(api.searchMovieByName, api, 'Full');
   // }, 14000);
 })();
+
+//----------------------- origen-----------------------
+// // do not remove the code below
+// import { cardList } from './card-draw.js';
+// import { LocalStorageEntry } from './localStorageEntry.js';
+// const localStorageCurrentPage = new LocalStorageEntry('current_page_number');
+
+// // To create a pagination you have to create an instance of class Pagination
+// // and call setFunction method with relevant arguments
+// export default class Pagination {
+//   constructor(isPopularFilms) {
+//     this.paginationPagesList = document.querySelector('.js-pages-list');
+//     this.paginationArrowPrev = document.querySelector('.js-prev-btn');
+//     this.paginationArrowNext = document.querySelector('.js-next-btn');
+
+//     this.#hidePagination();
+
+//     this.paginationPagesList.onclick = this.#onClick;
+//     this.paginationArrowPrev.onclick = this.#onPrev;
+//     this.paginationArrowNext.onclick = this.#onNext;
+
+//     this.page = 1;
+//     this.isPopularFilms = isPopularFilms;
+
+//     if (isPopularFilms) {
+//       this.page = this.#getPageFromLocalStorage() || 1;
+//       // this.#putPageToLocalStorage(1); // Each next query wiil be the 1st page
+//     }
+
+//     window.onbeforeunload = this.#onSave;
+//   }
+
+//   /** First param - link to requested function
+//    *
+//    * Second param - link to instance of requested function
+//    * (instance has to be with setter named "pageNumber" to set current page
+//    * and with getter named "totalPagesNumber" to get count of total pages in result)
+//    *
+//    * Third and further params - args of requested function
+//    */
+//   setFunction = async (fn, ...arrOfArgs) => {
+//     this.fn = fn.bind(...arrOfArgs);
+//     this.linkToIntance = arrOfArgs[0];
+//     this.#render();
+//   };
+
+//   #onSave = event => {
+//     event.preventDefault();
+//     if (this.isPopularFilms) this.#putPageToLocalStorage(this.page);
+//   };
+
+//   #onPrev = event => {
+//     this.page--;
+//     this.#render();
+//     this.#onTop();
+//   };
+
+//   #onNext = event => {
+//     this.page++;
+//     this.#render();
+//     this.#onTop();
+//   };
+
+//   #onClick = event => {
+//     this.classes = [...event.target.classList];
+//     if (this.classes.includes('pagination__dots')) return;
+//     if (this.classes.includes('pagination__list')) return;
+//     if (this.classes.includes('pagination__number_current')) return;
+
+//     this.page = Number(event.target.innerText);
+//     this.#render();
+//     this.#onTop();
+//   };
+
+//   #render = async () => {
+//     this.#hidePagination();
+
+//     this.linkToIntance.pageNumber = this.page;
+//     this.res = await this.fn();
+//     this.totalPages = this.linkToIntance.totalPagesNumber;
+//     cardList(this.res);
+
+//     if (this.isPopularFilms) this.#putPageToLocalStorage(this.page);
+
+//     this.#createPagination(this.page, this.totalPages);
+//   };
+
+//   #onTop() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+//   }
+
+//   #getPageFromLocalStorage() {
+//     return localStorageCurrentPage.getLocalStorageEntry();
+//   }
+
+//   #putPageToLocalStorage(currentPage) {
+//     localStorageCurrentPage.addPageNumberToLocalStorage(currentPage);
+//   }
+
+//   #hidePagination() {
+//     this.paginationPagesList.innerHTML = '';
+//     this.paginationArrowPrev.classList.add('visually-hidden');
+//     this.paginationArrowNext.classList.add('visually-hidden');
+//   }
+
+//   #createPagination(page, totalPages) {
+//     let markup = '';
+
+//     let beforeCurr = page - 2;
+//     let afterCurr = page + 2;
+
+//     if (page === 1) {
+//       afterCurr += 2;
+//     } else if (page === 2) {
+//       afterCurr += 1;
+//     }
+
+//     if (page === totalPages) {
+//       beforeCurr -= 2;
+//     } else if (page === totalPages - 1) {
+//       beforeCurr -= 1;
+//     }
+
+//     if (page > 1) {
+//       this.paginationArrowPrev.classList.remove('visually-hidden');
+//     } else {
+//       this.paginationArrowPrev.classList.add('visually-hidden');
+//     }
+
+//     if (page >= 4) {
+//       markup += `<li>
+//         <button type="button" class="pagination__item pagination__number except-mobile">
+//           1
+//         </button>
+//       </li>`;
+
+//       if (page >= 5) {
+//         markup += `<li class="pagination__item pagination__dots except-mobile">...</li>`;
+//       }
+//     }
+
+//     for (let pageNumber = beforeCurr; pageNumber <= afterCurr; pageNumber++) {
+//       if (pageNumber <= 0 || pageNumber > totalPages) continue;
+
+//       if (pageNumber === page) {
+//         markup += `<li>
+//         <button type="button" class="pagination__item pagination__number pagination__number_current">
+//           ${pageNumber}
+//         </button>
+//       </li>`;
+//         continue;
+//       }
+
+//       markup += `<li>
+//         <button type="button" class="pagination__item pagination__number">
+//           ${pageNumber}
+//         </button>
+//       </li>`;
+//     }
+
+//     if (page <= totalPages - 3) {
+//       if (page <= totalPages - 4) {
+//         markup += `<li class="pagination__item pagination__dots except-mobile">...</li>`;
+//       }
+
+//       markup += `<li>
+//         <button type="button" class="pagination__item pagination__number except-mobile">
+//           ${totalPages}
+//         </button>
+//       </li>`;
+//     }
+
+//     if (page === totalPages) {
+//       this.paginationArrowNext.classList.add('visually-hidden');
+//     } else {
+//       this.paginationArrowNext.classList.remove('visually-hidden');
+//     }
+
+//     this.paginationPagesList.innerHTML = markup;
+//   }
+// }
+
+// // Examples of using class below
+// import ApiMovies from './fetch.js';
+// const api = new ApiMovies();
+
+// //comment unneccessary code block
+// (async function (movieName = 'Avatar') {
+//   // // code for fetching trend movies and creating pagination
+//   // const pagination = new Pagination(true);
+//   // pagination.setFunction(api.fetchTrendMovies, api);
+//   //
+//   // // code for fetching requested movies and creating pagination
+//   // const pagination = new Pagination();
+//   // pagination.setFunction(api.searchMovieByName, api, movieName);
+//   //
+//   // setTimeout(async () => {
+//   //   const pagination = new Pagination();
+//   //   pagination.setFunction(api.searchMovieByName, api, movieName);
+//   // }, 7000);
+//   //
+//   // setTimeout(async () => {
+//   //   const pagination = new Pagination();
+//   //   pagination.setFunction(api.searchMovieByName, api, 'Full');
+//   // }, 14000);
+// })();
