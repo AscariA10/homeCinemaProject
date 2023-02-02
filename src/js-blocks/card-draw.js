2e026cb930d58d00bb08c3dd5accd87a8d428a75
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-02-02 13:55:39 +0200

    anim-scroll

--- src/js-blocks/card-draw.js
+++ src/js-blocks/card-draw.js
@@ -5,45 +5,48 @@ const galleryContainer = document.querySelector('.gallery-list');
 const api = new ApiMovies();
 
 const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const DEFAULT_IMG_URL = 'https://images.prom.ua/211029177_w640_h640_211029177.jpg';
+const DEFAULT_IMG_URL =
+  'https://images.prom.ua/211029177_w640_h640_211029177.jpg';
 
 export const cardList = data => {
-   const markup = data
-      .map(el => {
-         return cardContainer(el);
-      })
-      .join('');
+  const markup = data
+    .map(el => {
+      return cardContainer(el);
+    })
+    .join('');
 
-   galleryContainer.innerHTML = markup;
+  galleryContainer.innerHTML = markup;
 };
 
 export const cardContainer = ({
-   title,
-   release_date,
-   poster_path,
-   vote_average,
-   id,
-   genre_ids,
+  title,
+  release_date,
+  poster_path,
+  vote_average,
+  id,
+  genre_ids,
 }) => {
-   const getImgUrl = path => {
-      return path ? `${BASE_IMG_URL}${poster_path}` : DEFAULT_IMG_URL;
-   };
+  const getImgUrl = path => {
+    return path ? `${BASE_IMG_URL}${poster_path}` : DEFAULT_IMG_URL;
+  };
 
-   const getReleaseDate = releaseDate => {
-      return releaseDate ? new Date(release_date).getFullYear() : 'No information';
-   };
+  const getReleaseDate = releaseDate => {
+    return releaseDate
+      ? new Date(release_date).getFullYear()
+      : 'No information';
+  };
 
-   const imgUrl = getImgUrl(poster_path);
-   const releaseYear = getReleaseDate(release_date);
-   const average = vote_average.toFixed(1);
+  const imgUrl = getImgUrl(poster_path);
+  const releaseYear = getReleaseDate(release_date);
+  const average = vote_average.toFixed(1);
 
-   const genres = сhangeIdtoGener(genre_ids);
+  const genres = сhangeIdtoGener(genre_ids);
 
-   return renderCard(title, imgUrl, releaseYear, average, id, genres);
+  return renderCard(title, imgUrl, releaseYear, average, id, genres);
 };
 
 export const renderCard = (title, img, date, average, id, genres) => {
-   return `<li class="gallery-card list" data-id=${id}>
+  return `<li class="gallery-card list _anim-items _anim-no-hide" data-id=${id}>
     <a class='card-link' href="">    
         <img class='card-img' src="${img}" alt="${title}">
         <p class="card-title">${title}</p>      
