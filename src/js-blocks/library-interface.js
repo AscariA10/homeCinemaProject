532a5a67050f233cb442ee88313640b8aa9889c4
VolodymyrKravchenko <109697703+AscariA10@users.noreply.github.com>
2023-02-02 10:44:02 +0200

    minor changes

--- src/js-blocks/library-interface.js
+++ src/js-blocks/library-interface.js
@@ -20,135 +20,137 @@ watchedBtn.addEventListener('click', onWatched);
 queueBtn.addEventListener('click', onQueue);
 
 class LibraryRender {
-  constructor() {
-    this.page = 1;
+   constructor() {
+      this.page = 1;
 
-    this.resize();
+      this.resize();
 
-    this.calculateTotalPages();
-  }
+      this.calculateTotalPages();
+   }
 
-  /** Returns page number for pagination */
-  get pageNumber() {
-    return this.page;
-  }
+   /** Returns page number for pagination */
+   get pageNumber() {
+      return this.page;
+   }
 
-  /** sets the page number if the user clicks on the page number in the pagination */
-  set pageNumber(number) {
-    this.page = number;
-  }
+   /** sets the page number if the user clicks on the page number in the pagination */
+   set pageNumber(number) {
+      this.page = number;
+   }
 
-  /** Returns total pages number for pagination */
-  get totalPagesNumber() {
-    return this.totalPages;
-  }
+   /** Returns total pages number for pagination */
+   get totalPagesNumber() {
+      return this.totalPages;
+   }
 
-  resize() {
-    const viewportWidth = window.innerWidth;
+   resize() {
+      const viewportWidth = window.innerWidth;
 
-    if (viewportWidth <= 767) this.countFilmsPerPage = 4;
-    if (viewportWidth >= 768) this.countFilmsPerPage = 8;
-    if (viewportWidth >= 1280) this.countFilmsPerPage = 9;
-  }
+      if (viewportWidth <= 767) this.countFilmsPerPage = 4;
+      if (viewportWidth >= 768) this.countFilmsPerPage = 8;
+      if (viewportWidth >= 1280) this.countFilmsPerPage = 9;
+   }
 
-  calculateTotalPages() {
-    this.movies = this.getMoviesFromLocalStorage();
-    const totalLength = this.movies.length;
-    const totalPages = Math.ceil(totalLength / this.countFilmsPerPage);
-    this.totalPages = totalPages;
-  }
+   calculateTotalPages() {
+      this.movies = this.getMoviesFromLocalStorage();
+      const totalLength = this.movies.length;
+      const totalPages = Math.ceil(totalLength / this.countFilmsPerPage);
+      this.totalPages = totalPages;
+   }
 
-  getMovies() {
-    const movies = [...this.movies].splice(
-      (this.page - 1) * this.countFilmsPerPage,
-      this.countFilmsPerPage
-    );
-    return movies;
-  }
-  renderRecommendedFilms = async () => {
-    const response = await apiMovies.fetchTrendMovies();
-    cardList([...response].splice(0, this.countFilmsPerPage));
-  };
+   getMovies() {
+      const movies = [...this.movies].splice(
+         (this.page - 1) * this.countFilmsPerPage,
+         this.countFilmsPerPage
+      );
+      return movies;
+   }
+   renderRecommendedFilms = async () => {
+      const response = await apiMovies.fetchTrendMovies();
+      cardList([...response].splice(0, this.countFilmsPerPage));
+   };
 }
 
 class LibraryRenderWatched extends LibraryRender {
-  getMoviesFromLocalStorage() {
-    return watchedMoviesStorage.getLocalStorageEntry() || [];
-  }
+   getMoviesFromLocalStorage() {
+      return watchedMoviesStorage.getLocalStorageEntry() || [];
+   }
 
-  renderRecommended = async () => {
-    Notify.info("You haven't added any movie to watched yet. Let's do it!");
-    const markup = `<span class="recommended-films">Recommended films</span>`;
-    recommendedData.innerHTML = markup;
-    this.renderRecommendedFilms();
-  };
+   renderRecommended = async () => {
+      Notify.info("You haven't added any movie to watched yet. Let's do it!");
+      const markup = `<span class="recommended-films">Recommended films</span>`;
+      recommendedData.innerHTML = markup;
+      this.renderRecommendedFilms();
+   };
 }
 
 class LibraryRenderQueue extends LibraryRender {
-  getMoviesFromLocalStorage() {
-    return queueMoviesStorage.getLocalStorageEntry() || [];
-  }
+   getMoviesFromLocalStorage() {
+      return queueMoviesStorage.getLocalStorageEntry() || [];
+   }
 
-  renderRecommended = async () => {
-    Notify.info("You haven't added any movie to queue yet. Let's do it!");
-    const markup = `<span class="recommended-films">Recommended films</span>`;
-    recommendedData.innerHTML = markup;
-    this.renderRecommendedFilms();
-  };
+   renderRecommended = async () => {
+      Notify.info("You haven't added any movie to queue yet. Let's do it!");
+      const markup = `<span class="recommended-films">Recommended films</span>`;
+      recommendedData.innerHTML = markup;
+      this.renderRecommendedFilms();
+   };
 }
 
 async function onWatched(event) {
-  queueBtn.style.backgroundColor = 'transparent';
-  watchedBtn.style.backgroundColor = '#ff6b01';
+   queueBtn.style.backgroundColor = 'transparent';
+   watchedBtn.style.backgroundColor = '#ff6b01';
 
-  const watchedMovies = new LibraryRenderWatched();
-  const pagination = new Pagination();
+   const watchedMovies = new LibraryRenderWatched();
+   const pagination = new Pagination();
 
-  if (!watchedMovies.getMovies().length) {
-    await watchedMovies.renderRecommended();
-    window.onresize = debounce(function () {
+   if (!watchedMovies.getMovies().length) {
+      await watchedMovies.renderRecommended();
+      window.onresize = debounce(function () {
+         watchedMovies.resize();
+         watchedMovies.renderRecommended();
+      }, 500);
+      return;
+   }
+
+   recommendedData.innerHTML = '';
+   pagination.setFunction(watchedMovies.getMovies, watchedMovies);
+
+   const debouncedSetFunction = debounce(() => {
+      pagination.page = 1;
       watchedMovies.resize();
-      watchedMovies.renderRecommended();
-    }, 500);
-    return;
-  }
-
-  recommendedData.innerHTML = '';
-  pagination.setFunction(watchedMovies.getMovies, watchedMovies);
-
-  const debouncedSetFunction = debounce(() => {
-    pagination.page = 1;
-    watchedMovies.resize();
-    watchedMovies.calculateTotalPages();
-    pagination.setFunction(watchedMovies.getMovies, watchedMovies);
-  }, 500);
-  window.onresize = debouncedSetFunction;
+      watchedMovies.calculateTotalPages();
+      pagination.setFunction(watchedMovies.getMovies, watchedMovies);
+   }, 500);
+   window.onresize = debouncedSetFunction;
 }
 
 async function onQueue(event) {
-  watchedBtn.style.backgroundColor = 'transparent';
-  queueBtn.style.backgroundColor = '#ff6b01';
+   watchedBtn.style.backgroundColor = 'transparent';
+   queueBtn.style.backgroundColor = '#ff6b01';
 
-  const queueMovies = new LibraryRenderQueue();
-  const pagination = new Pagination();
+   const queueMovies = new LibraryRenderQueue();
+   const pagination = new Pagination();
 
-  if (!queueMovies.getMovies().length) {
-    await queueMovies.renderRecommended();
-    window.onresize = debounce(function () {
+   if (!queueMovies.getMovies().length) {
+      await queueMovies.renderRecommended();
+      window.onresize = debounce(function () {
+         queueMovies.resize();
+         queueMovies.renderRecommended();
+      }, 500);
+      return;
+   }
+
+   recommendedData.innerHTML = '';
+   pagination.setFunction(queueMovies.getMovies, queueMovies);
+
+   const debouncedSetFunction = debounce(() => {
+      pagination.page = 1;
       queueMovies.resize();
-      queueMovies.renderRecommended();
-    }, 500);
-    return;
-  }
-
-  recommendedData.innerHTML = '';
-  pagination.setFunction(queueMovies.getMovies, queueMovies);
-
-  const debouncedSetFunction = debounce(() => {
-    pagination.page = 1;
-    queueMovies.resize();
-    queueMovies.calculateTotalPages();
-    pagination.setFunction(queueMovies.getMovies, queueMovies);
-  }, 500);
-  window.onresize = debouncedSetFunction;
+      queueMovies.calculateTotalPages();
+      pagination.setFunction(queueMovies.getMovies, queueMovies);
+   }, 500);
+   window.onresize = debouncedSetFunction;
 }
+
+import '../js-blocks/team-window';
